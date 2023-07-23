a4b67088fef5568625af8a30f115bfea9a872cce
Gabriel Nascimento dos Santos <gndsantos@gmail.com>
2023-07-23 14:30:42 -0300

    Initial commit

---
+++ globoesporte/pipelines.py
@@ -0,0 +1,76 @@
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+# useful for handling different item types with a single interface
+import json
+import logging
+import sqlite3
+import pymongo
+
+class GloboesportePipeline:
+
+    def open_spider(self, sider):
+        logging.warning('***************** SPIDER OPENED - PIPELINE *****************')
+
+    def close_spider (self, spider):
+        logging.warning('***************** SPIDER CLOSED - PIPELINE *****************')
+
+    def process_item(self, item, spider):
+        return item
+    
+class MongodbPipeline:
+    collection_name = 'articles'
+
+    def open_spider(self, spider):
+        self.client = pymongo.MongoClient("mongodb+srv://globoesporte_webscraping:gPbuLCGnbnH2ApAG@globoesporte.o6lbwjs.mongodb.net/?retryWrites=true&w=majority")
+        self.db = self.client['GloboEsporte_Scraped_Data']
+
+    def close_spider(self, spider):
+        self.client.close()
+
+    def process_item(self, item, spider):
+        self.db[self.collection_name].insert_one(dict(item))
+        return item
+
+class SQLitePipeline:
+    collection_name = 'articles'
+
+    def open_spider(self, spider):
+        self.connection = sqlite3.connect("ge_articles.db")
+        self.c = self.connection.cursor()
+
+        try:
+            self.c.execute('''
+                CREATE TABLE ge_transcripts(
+                    title TEXT,
+                    subtitle TEXT,
+                    author TEXT,
+                    text TEXT,
+                    quotes TEXT
+                )
+            ''')
+            self.connection.commit()
+        except sqlite3.OperationalError:
+            pass 
+
+    def close_spider(self, spider):
+        self.connection.close()
+
+    def process_item(self, item, spider):
+        # Convert the quotes list of dictionaries to a JSON string
+        item['quotes'] = json.dumps(item['quotes'])
+
+        self.c.execute('''
+            INSERT INTO ge_transcripts (title, subtitle, author, text, quotes) VALUES (?, ?, ?, ?, ?)
+        ''', (
+            item['title'],
+            item['subtitle'],
+            item['author'],
+            item['text'],
+            item['quotes'],
+        ))
+        self.connection.commit()
+        return item
---
+++ globoesporte/spiders/globoesporte.py
@@ -0,0 +1,50 @@
+from ..items import JournalItem
+import scrapy
+import scrapy
+
+
+class GloboesporteSpider(scrapy.Spider):
+    name = "globoesporte"
+    allowed_domains = ["ge.globo.com"]
+    start_urls = ["https://ge.globo.com"]
+    
+
+    def parse(self, response):
+        bastian_page = response.xpath('//div[@class="feed-post-body"]')
+
+        for bastian_feed_item in bastian_page:
+            bf_urls = bastian_feed_item.xpath('div[@class="feed-post-body-title gui-color-primary gui-color-hover "]/div[@class="_evt"]/a/@href').getall()
+
+            for bf_url in bf_urls:
+                yield scrapy.Request(url=bf_url, callback=self.parse_article)
+
+    def parse_article(self, response):
+
+            title = response.xpath('//h1[@class="content-head__title"]/text()').get()
+            subtitle = response.xpath('//h2[@class="content-head__subtitle"]/text()').get()
+
+            author = response.xpath('//p[@class="content-publication-data__from"]/text()').get()
+            author = author.strip('Por')
+
+            city = response.xpath('//p[@class="content-publication-data__from"]/span/text()').get()
+            city = city.strip('— ')
+
+            text_parts = response.css('p.content-text__container *::text').getall()
+            text = ' '.join(text_parts).strip()
+
+            quote_text = response.xpath('//div[@class="quote__caption"]/span/text()').extract()
+            quote_author = response.xpath('//div[@class="quote__author"]/span/text()').extract()
+            if len(quote_text) == len(quote_author):
+                    quotes = [{'text': text, 'author': author} for text, author in zip(quote_text, quote_author)]
+    
+
+
+            item = JournalItem(
+            title=title,
+            subtitle=subtitle,
+            author=author,
+            city=city,
+            text=text,
+            quotes=quotes,
+        )
+            yield item
